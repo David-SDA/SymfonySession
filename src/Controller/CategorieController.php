6a5fb2d96e7752eedf8a4ff0732b07b71dd2db63
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-23 15:59:13 +0200

    Ajout listage des modules d'une catégorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
 use App\Repository\CategorieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CategorieController extends AbstractController
 {
     #[Route('/categorie', name: 'app_categorie')]
-    public function index(CategorieRepository $categorieRepository): Response
-    {
+    public function index(CategorieRepository $categorieRepository): Response {
+
         /* Trouver toutes les catégories */
         $categories = $categorieRepository->findBy([], ["libelle" => "ASC"]);
         return $this->render('categorie/index.html.twig', [
             'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/categorie/{id}', name: 'show_categorie')]
+    public function show(Categorie $categorie): Response{
+
+        return $this->render('categorie/show.html.twig', [
+            'categorie' => $categorie
         ]);
     }
 }
