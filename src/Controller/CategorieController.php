8c07ab653fd984b486b155a586bb9b76b1eae1e0
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-24 11:26:35 +0200

    Ajout, formulaire d'ajout/modification, catégorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
+use App\Form\CategorieType;
 use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +22,33 @@ class CategorieController extends AbstractController
             'categories' => $categories,
         ]);
     }
+
+
+    #[Route('categorie/add', name: 'add_categorie')]
+    #[Route('/categorie/{id}/edit', name: 'edit_categorie')]
+    public function add(EntityManagerInterface $entityManager, Categorie $categorie = null, Request $request): Response{
+        
+        /* Si la categorie n'existe pas, on le crée */
+        if(!$categorie){
+            $categorie = new Categorie();
+        }
+        
+        $form = $this->createForm(CategorieType::class, $categorie); // On crée le formulaire
+        $form->handleRequest($request); // Inspecte la requête
+        if($form->isSubmitted() && $form->isValid()){
+            $categorie = $form->getData(); // On récupère les données
+            $entityManager->persist($categorie); // Équivalent du prepare
+            $entityManager->flush(); // Équivalent du execute
+
+            return $this->redirectToRoute("app_categorie");
+        }
+
+        return $this->render('categorie/add.html.twig', [
+            'formAddCategorie' => $form->createView(),
+            'edit' => $categorie->getId()
+        ]);
+    }
+
 
     #[Route('/categorie/{id}', name: 'show_categorie')]
     public function show(Categorie $categorie): Response{
