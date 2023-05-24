c9904dc92755e3b1a835e631fbb74950409fdce8
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-24 12:03:41 +0200

    Ajout formulaire ajout/modification module

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Module;
+use App\Form\ModuleType;
+use App\Entity\Categorie;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,6 +20,34 @@ class ModuleController extends AbstractController
             'controller_name' => 'ModuleController',
         ]);
     }
+
+
+    #[Route('module/add/{id}', name: 'add_module')]
+    #[Route('/module/{id}/edit', name: 'edit_module')]
+    public function add(EntityManagerInterface $entityManager, Module $module = null, Categorie $categorie, Request $request): Response{
+        
+        /* Si le module n'existe pas, on le crée */
+        if(!$module){
+            $module = new Module();
+        }
+        
+        $form = $this->createForm(ModuleType::class, $module); // On crée le formulaire
+        $form->handleRequest($request); // Inspecte la requête
+        if($form->isSubmitted() && $form->isValid()){
+            $module = $form->getData(); // On récupère les données
+            $entityManager->persist($module); // Équivalent du prepare
+            $entityManager->flush(); // Équivalent du execute
+
+            return $this->redirectToRoute("app_module");
+        }
+
+        return $this->render('module/add.html.twig', [
+            'formAddModule' => $form->createView(),
+            'categorie' => $categorie,
+            'edit' => $module->getId()
+        ]);
+    }
+
 
     #[Route('/module/{id}', name: 'show_module')]
     public function show(Module $module): Response{
