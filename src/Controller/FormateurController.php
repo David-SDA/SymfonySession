be3b6ae4f4c802c1b85c44942f905da91d3ce969
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-31 16:22:27 +0200

    Ajout de l'édition et l'ajout d'un formateur

--- src/Controller/FormateurController.php
+++ src/Controller/FormateurController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Formateur;
+use App\Form\FormateurType;
 use App\Repository\FormateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,6 +21,33 @@ class FormateurController extends AbstractController
             'formateurs' => $formateurs,
         ]);
     }
+
+
+    #[Route('formateur/add', name: 'add_formateur')]
+    #[Route('/formateur/{id}/edit', name: 'edit_formateur')]
+    public function add(EntityManagerInterface $entityManager, Formateur $formateur = null, Request $request): Response{
+        
+        /* Si le formateur n'existe pas, on le crée */
+        if(!$formateur){
+            $formateur = new Formateur();
+        }
+        
+        $form = $this->createForm(FormateurType::class, $formateur); // On crée le formulaire
+        $form->handleRequest($request); // Inspecte la requête
+        if($form->isSubmitted() && $form->isValid()){
+            $formateur = $form->getData(); // On récupère les données
+            $entityManager->persist($formateur); // Équivalent du prepare
+            $entityManager->flush(); // Équivalent du execute
+
+            return $this->redirectToRoute("app_formateur");
+        }
+
+        return $this->render('formateur/add.html.twig', [
+            'formAddFormateur' => $form->createView(),
+            'edit' => $formateur->getId()
+        ]);
+    }
+
 
     #[Route('formateur/{id}', name: 'show_formateur')]
     public function show(Formateur $formateur): Response{
