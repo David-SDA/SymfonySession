4891a769701b931e9b4f803c49aa93789d7dfbb3
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-05-31 16:01:49 +0200

    Ajout du listage des formateurs

--- src/Controller/FormateurController.php
+++ src/Controller/FormateurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FormateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class FormateurController extends AbstractController
 {
     #[Route('/formateur', name: 'app_formateur')]
-    public function index(): Response
-    {
+    public function index(FormateurRepository $formateurRepository): Response{
+        /* Trouver touts les formateurs */
+        $formateurs = $formateurRepository->findBy([], ["nom" => "ASC"]);
         return $this->render('formateur/index.html.twig', [
-            'controller_name' => 'FormateurController',
+            'formateurs' => $formateurs,
         ]);
     }
 }
